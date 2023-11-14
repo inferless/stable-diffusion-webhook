90276a822df421b9b6f3186851bf73e542de3f8e
Nilesh Agarwal <nick.aggarwal@gmail.com>
2023-11-14 12:07:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,26 +2,30 @@ from diffusers import StableDiffusionPipeline
 import torch
 from io import BytesIO
 import base64
-from huggingface_hub import snapshot_download
 import os
+import requests
+
+WEBHOOK_URL = "http://your-webhook-url.com" # You can also use os.env.get("WEBHOOkURL")
 
 class InferlessPythonModel:
     def initialize(self):
-        AWS_KEY = os.environ.get('AWS_KEY', "not hello" )
-        AWS_SECRET = os.environ.get('AWS_SECRET' ,  "not world" )
-        print("Value is " + AWS_KEY + " and " + AWS_SECRET)
         self.pipe = StableDiffusionPipeline.from_pretrained(
             "runwayml/stable-diffusion-v1-5",
             torch_dtype=torch.float16,
             device_map='auto'
         )
+        
     def infer(self, inputs):
         prompt = inputs["prompt"]
         image = self.pipe(prompt).images[0]
         buff = BytesIO()
         image.save(buff, format="JPEG")
-        img_str = base64.b64encode(buff.getvalue()).decode()
-        return { "generated_image_base64" : img_str }
+        img_str = base64.b64encode(buff.get value()).decode()
+        data = { "generated_image_base64" : img_str }
+        // Call the Webhook 
+        response = requests.post(WEBHOOK_URL, json=response)
+        
+        return data
 
     def finalize(self):
         self.pipe = None

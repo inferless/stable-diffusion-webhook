cdc769322d20b6a30a7880f20e835bedabc4736e
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-13 17:18:31 +0530

    Add files via upload

---
+++ input_schema.py
@@ -0,0 +1,8 @@
+INPUT_SCHEMA = {
+    "prompt": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["a black cat with glowing eyes, cute, adorable, disney, pixar, highly detailed, 8k"]
+    }
+}
